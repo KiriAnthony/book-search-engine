7342bb965af5cda15a38c11404758cdd588b9105
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-05-31 12:23:28 -0700

    update queries.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,17 @@
 const { User } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ id: context.user._id })
+                    .select('-__v -password')
                     .populate('books')
+                return userData;
             }
-            return userData;
+            throw new AuthenticationError('Not logged in');
         }
     }
 };
